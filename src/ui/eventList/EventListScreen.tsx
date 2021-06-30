a3b3038e2ff09a8ccb64cae13178cacb1c790b92
gaphg <70362827+gaphg@users.noreply.github.com>
2021-06-29 17:05:48 -0700

    Update EventListScreen.tsx

--- src/ui/eventList/EventListScreen.tsx
+++ src/ui/eventList/EventListScreen.tsx
@@ -1,6 +1,34 @@
 import React from "react";
-import {Button, SafeAreaView, StatusBar, StyleSheet, Text,} from "react-native";
+import {Button, SafeAreaView, StatusBar, StyleSheet, Text, FlatList, View} from "react-native";
 import {useNavigation, StackActions} from "@react-navigation/native";
+
+// basic event class
+class Event {
+  name: string;
+  date: Date;
+  constructor(name: string, date: Date) {
+    this.name = name;
+    this.date = date;
+  }
+}
+
+// array of fake events to render
+const fakeEvents = [
+  new Event('event1', new Date()),
+  new Event('event2', new Date()),
+  new Event('event3', new Date()),
+  new Event('event4', new Date()),
+];
+
+// return an event component thing given an event
+function makeEventComponent(event: Event) {
+  return (
+    <View>
+      <Text>{event.name}</Text>
+      <Button title="Go to Event Detail" onPress={(ev) => {} } />
+    </View>
+  );
+}
 
 
 export function EventListScreen() {
@@ -8,10 +36,8 @@ export function EventListScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar hidden />
-      <Text>Hello world Splash</Text>
-      <Button title="Go to Event Detail" onPress={ (event) =>
-        {navigation.dispatch(StackActions.push("EventDetail"));}
-      } />
+      <Text>Upcoming Events</Text>
+      <FlatList data={fakeEvents} renderItem={({item}) => makeEventComponent(item)} keyExtractor={item => item.name}/>
     </SafeAreaView>
   );
 }
