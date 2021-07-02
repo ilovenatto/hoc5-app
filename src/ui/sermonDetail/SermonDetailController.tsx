f4ae46e196d19b5d5813dddaabb664cbb502134f
Johnny Chen <johnny.chen@clever.com>
2021-07-02 09:38:05 -0700

    Added bottom tab nav for Sermons and Events
    * Added bottom tab nav for Sermons and Events
    * Added react-navigation/bottom-tabs
    * Added placeholder screens for SermonList and SermonDetail
    * Killed Splash screen
    
    TEST
    - [x] Viewed new tabs on Expo via Web browser mode

---
+++ src/ui/sermonDetail/SermonDetailController.tsx
@@ -0,0 +1,10 @@
+import React, {useEffect} from "react";
+import {SermonDetailScreen} from "./SermonDetailScreen";
+import {LOG} from "../../util/HocLogger";
+
+/**
+ * ViewController for {@link SermonDetailScreen}
+ */
+export function SermonDetailController() {
+  return <SermonDetailScreen />;
+}
---
+++ src/ui/sermonList/SermonListController.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {SermonListScreen} from "./SermonListScreen";
+
+/**
+ * ViewController for {@link EventListScreen}
+ */
+export function SermonListController() {
+  return <SermonListScreen />;
+}
