680b50749ed6558ccd1226567c669c5c46b8be51
CadenChou <85153511+CadenChou@users.noreply.github.com>
2021-09-14 18:15:27 -0700

    Added ability to pass sermon index to different screens

--- src/ui/sermonDetail/SermonDetailController.tsx
+++ src/ui/sermonDetail/SermonDetailController.tsx
@@ -1,28 +1,32 @@
-import React, {useEffect} from "react";
+
+
+import React, {useContext, useState} from "react"
+import { AppContext } from "../../data/AppContext";
 import {SermonDetailScreen} from "./SermonDetailScreen";
 import {LOG} from "../../util/HocLogger";
-import { useNavigation } from "@react-navigation/core";
-import { useRoute } from "@react-navigation/native";
-
+import {useRoute} from "@react-navigation/native"
+ 
 /**
- * ViewController for {@link SermonDetailScreen1}
- */
-
+* ViewController for {@link SermonDetailScreen}
+*/
+ 
+ 
+ 
 interface SermonDetailRouteProp {
-  key: string,
-  name: string,
-  params: {
-    youtubeVidID: string,
-    notesURL: string
-  }
+ key: string,
+ name: string,
+ params: {
+   sermonIndex: number
+ }
 }
-
+ 
+ 
 export function SermonDetailController() {
-
-  const params = useRoute<SermonDetailRouteProp>().params;
-
-  LOG.debug(params.youtubeVidID);
-
-
-  return <SermonDetailScreen />;
+ 
+ // First, we receive the index of the sermon from the Sermon List
+ const params = useRoute<SermonDetailRouteProp>().params;
+ 
+ // Here, we call the Sermon Detail Screen, passing the sermonIndex parameter
+ return <SermonDetailScreen sermonIndex={params.sermonIndex} />;
 }
+
